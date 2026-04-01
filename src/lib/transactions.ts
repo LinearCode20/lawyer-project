955dfb76aa6cd64b6502ecb0a5d92c4e091130cd
Mukesh <coddermk@gmail.com>
2026-04-01 22:48:43 +0530

    create customer on stripe while free-subscriber is added, save data into DB

--- src/lib/transactions.ts
+++ src/lib/transactions.ts
@@ -1,5 +1,42 @@
 import { supabase } from "@/lib/supabase/server";
 
+/**
+ * Save data for free sample user info
+ */
+type SubscribeFree = {
+  full_name: string;
+  email: string;
+  firm_name: string;
+  plan_type: string;
+  selected_areas: string;
+  stripe_customer_id: string;
+  created_at: string;
+};
+
+export async function SubscribeFree(data: SubscribeFree) {
+  const { error } = await supabase
+    .from("free_subscribe_request")
+    .insert([data]);
+
+  if (error) {
+    console.error("Supabase insert error:", error);
+    throw new Error("Failed to save information.");
+  }
+}
+
+
+export async function getSubscriberByEmail(email: string) {
+  const { data } = await supabase
+    .from("free_subscribe_request")
+    .select("*")
+    .eq("email", email)
+    .single();
+
+  return data;
+}
+
+
+
 type CaptureEmail = {
   email: string;
   areas_of_law: string;
@@ -41,24 +78,4 @@ export async function FreeSampleData(data: FreeSample) {
 }
 
 
-/**
- * Save data for free sample user info
- */
-type SubscribeFree = {
-  full_name: string;
-  email: string;
-  firm_name: string;
-  selected_areas: string;
-  created_at: string;
-};
 
-export async function SubscribeFree(data: SubscribeFree) {
-  const { error } = await supabase
-    .from("free_subscribe_request")
-    .insert([data]);
-
-  if (error) {
-    console.error("Supabase insert error:", error);
-    throw new Error("Failed to save information.");
-  }
-}
