b777261e1f524e8702888d1e8418749ba9a299e8
nikhilsohil <sohilnikhil@gmail.com>
2026-05-02 22:40:35 +0530

    update changes

--- src/components/bottom-banner.tsx
+++ src/components/bottom-banner.tsx
@@ -7,17 +7,17 @@ export default function BottomBanner() {
     <section className="py-32 bg-primary text-white">
       <div className="md:max-w-3xl mx-auto px-4 text-center">
         <span className="inline-block text-center bg-secondary w-20 h-px mb-4" />
-        <h1 className="text-4xl  mb-6 font-serif">
+        <h1 className="text-4xl  mb-4 font-serif">
           You do not know which of your current files are defensible.
         </h1>
-        <p className="text-xl text-gray-400 mb-4 max-w-3xl mx-auto">
+        <p className="text-xl text-gray-400  max-w-3xl mx-auto">
           This must now be tested across your current files.{" "}
         </p>
         <p className=" text-gray-400 mb-4 max-w-3xl mx-auto">
           There is no alternative way to establish your position.
         </p>
 
-        <div className="flex flex-col sm:flex-row gap-4 mt-8 justify-center mb-8 md:w-[70%] mx-auto">
+        <div className="flex flex-col sm:flex-row gap-4 mt-4 justify-center mb-8 md:w-[70%] mx-auto">
           <Link href="/#subscribe" rel="noopener noreferrer">
             <Button size="lg" variant="secondary" className=" px-14 w-full">
               <ArrowDownToLine className="mr-2" />
